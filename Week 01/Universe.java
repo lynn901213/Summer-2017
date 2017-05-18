d5f310f0ae21cafdf4d7f2e7182fd70e33e96331
cse360asu <cse360asu@gmail.com>
2017-05-18 00:20:04 -0700

    Update Universe.java

--- Week 01/Universe.java
+++ Week 01/Universe.java
@@ -5,3 +5,29 @@
  * @author Manohara Rao Penumala
  */
  
+package CSE360;
+
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+
+public class Universe extends JFrame {
+
+    public Universe () {
+     GridLayout grid = new GridLayout (3,2);   
+     setLayout(grid);
+     this.add(new Team0());
+     this.add(new Team0());
+     this.add(new Team0());
+     this.add(new Team0());
+     this.add(new Team0());
+     this.add(new Team0());
+    }
+
+    public static void main(String[] args) {
+        Universe u = new Universe();
+        u.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        u.setSize(500, 500);
+        u.setVisible(true);
+    }
+    
+}
